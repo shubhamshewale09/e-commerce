3be608d47f7a60b74211598f5c1ecbbb28944624
shubhamshewale09 <shewaleshubham123@gmail.com>
2024-12-03 23:16:32 +0530

    created new app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './component/Navbar';
+
+function App() {
+  return (
+    
+  <>
+<Navbar title="My App1" select1="select"/>
+  </> 
+  );
+}
+
+export default App;
